d40e678cfd94d002a769d72a68dad1e02b69a353
Thorsten Otto <admin@tho-otto.de>
2017-10-14 15:12:30 +0200

    Add script for creating large base packages

---
+++ mkbig.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+downloads=$HOME/webgo24/home/www/download/mint
+date=`date +%Y%m%d`
+here=`pwd`
+
+TAR=${TAR-tar}
+TAR_OPTS=${TAR_OPTS---owner=0 --group=0}
+
+for sys in cygwin32 mingw32 linux macos; do
+	d=`mktemp -d`
+	for pkg in "gcc-4.6.4-mint-*-bin-$sys.tar.xz" \
+		"binutils-*-mint-*-bin-$sys.tar.xz" \
+		"binutils-*-mintelf-*-bin-$sys.tar.xz" \
+		"mintbin-*-mint-*-bin-$sys.tar.xz" \
+		"gcc-7.2.0-mint-*-bin-$sys.tar.xz" \
+		"gcc-7.2.0-mintelf-*-bin-$sys.tar.xz" \
+		"gemlib-*-mint-*-dev.tar.xz" \
+		"gemlib-*-mintelf-*-dev.tar.xz" \
+		"mintlib-*-mint-*-dev.tar.xz" \
+		"mintlib-*-mintelf-*-dev.tar.xz" \
+		"pml-*-mint-*-dev.tar.xz" \
+		"pml-*-mintelf-*-dev.tar.xz" \
+		"cflib-*-mint-*-dev.tar.xz" \
+		"cflib-*-mintelf-*-dev.tar.xz" ; do
+		file=`echo $downloads/$pkg 2>/dev/null`
+		if test -f "$file"; then
+			if test -s "$file"; then
+				: echo "got $file"
+				tar -C "$d" -xf "$file"
+			else
+				echo "empty: $file" >&2
+			fi
+		else
+			echo "missing: $pkg" >&2
+			exit 1
+		fi
+	done
+	cd "$d"
+	case $sys in
+	mingw32)
+		cp -ar usr/. mingw32/.
+		rm -rf usr
+		;;
+	mingw64)
+		cp -ar usr/. mingw64/.
+		rm -rf usr
+		;;
+	macos)
+		cp -ar usr/. opt/cross-mint/.
+		rm -rf usr
+		;;
+	esac
+	bigfile="$downloads/m68k-atari-mint-base-$date-$sys.tar.xz"
+	${TAR} ${TAR_OPTS} -Jcf "$bigfile" *
+	cd "$here"
+	rm -rf "$d"
+	echo "created $bigfile"
+done
