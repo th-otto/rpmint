2fba5629a0050d75159110626cbd08f66905b49c
Thorsten Otto <admin@tho-otto.de>
2017-10-14 19:36:22 +0200

    Add script to compile LDG

---
+++ build-ldg.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+me="$0"
+scriptdir=${0%/*}
+
+PACKAGENAME=ldg
+VERSION=-svn-20171014
+VERSIONPATCH=
+
+. ${scriptdir}/functions.sh
+
+MINT_BUILD_DIR="$srcdir/src/devel"
+
+PATCHES="patches/ldg/cross.patch"
+
+unpack_archive
+
+cd "$MINT_BUILD_DIR"
+
+COMMON_CFLAGS="-W -Wall -O2 -fomit-frame-pointer -I../../include -I.. -I. $LTO_CFLAGS"
+
+make -f gcc.mak CROSS_PREFIX=${TARGET}- CFLAGS="-m68020-60 $COMMON_CFLAGS"
+mkdir -p "${THISPKG_DIR}${sysroot}${TARGET_LIBDIR}/m68020-60"
+cp -a ../../lib/gcc/libldg.a "${THISPKG_DIR}${sysroot}${TARGET_LIBDIR}/m68020-60"
+make -f gcc.mak clean
+
+make -f gcc.mak CROSS_PREFIX=${TARGET}- CFLAGS="-mcpu=5475 $COMMON_CFLAGS"
+mkdir -p "${THISPKG_DIR}${sysroot}${TARGET_LIBDIR}/m5475"
+cp -a ../../lib/gcc/libldg.a "${THISPKG_DIR}${sysroot}${TARGET_LIBDIR}/m5475"
+make -f gcc.mak clean
+
+make -f gcc.mak CROSS_PREFIX=${TARGET}- CFLAGS="-m68000 $COMMON_CFLAGS"
+mkdir -p "${THISPKG_DIR}${sysroot}${TARGET_LIBDIR}/m5475"
+cp -a ../../lib/gcc/libldg.a "${THISPKG_DIR}${sysroot}${TARGET_LIBDIR}"
+make -f gcc.mak clean
+
+mkdir -p "${THISPKG_DIR}${sysroot}${TARGET_PREFIX}/include"
+cp -a ../../include/ldg.h "${THISPKG_DIR}${sysroot}${TARGET_PREFIX}/include"
+
+make_archives
