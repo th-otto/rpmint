037a99eaf3f10425a7139a88114fce2a331f1e48
Thorsten Otto <admin@tho-otto.de>
2018-01-26 15:35:14 +0100

    Add stack size for bzip2

--- build-bzip2.sh
+++ build-bzip2.sh
@@ -57,29 +57,27 @@ rm -rf autom4te.cache config.h.in.orig
 cd "$MINT_BUILD_DIR"
 
 COMMON_CFLAGS="-O2 -fomit-frame-pointer $LTO_CFLAGS"
+STACKSIZE="-Wl,-stack,256k"
 
 CONFIGURE_FLAGS="--host=${TARGET} --prefix=${prefix}"
 
 export PKG_CONFIG_LIBDIR="$prefix/$TARGET/lib/pkgconfig"
 export PKG_CONFIG_PATH="$PKG_CONFIG_LIBDIR"
 
-CFLAGS="-m68020-60 $COMMON_CFLAGS" "$srcdir/configure" ${CONFIGURE_FLAGS} --libdir='${exec_prefix}/lib/m68020-60'
-${MAKE} $JOBS || exit 1
-${MAKE} DESTDIR="${THISPKG_DIR}${sysroot}" install || exit 1
-${MAKE} distclean
-make_bin_archive 020
+for CPU in ${ALL_CPUS}; do
+	eval CPU_CFLAGS=\${CPU_CFLAGS_$CPU}
+	eval multilibdir=\${CPU_LIBDIR_$CPU}
+	eval multilibexecdir=\${CPU_LIBEXECDIR_$CPU}
+	CFLAGS="$CPU_CFLAGS $COMMON_CFLAGS" \
+	LDFLAGS="$CPU_CFLAGS $COMMON_CFLAGS ${STACKSIZE}" \
+	"$srcdir/configure" ${CONFIGURE_FLAGS} \
+	--libdir='${exec_prefix}/lib'$multilibdir
 
-CFLAGS="-mcpu=5475 $COMMON_CFLAGS" "$srcdir/configure" ${CONFIGURE_FLAGS} --libdir='${exec_prefix}/lib/m5475'
-${MAKE} $JOBS || exit 1
-${MAKE} DESTDIR="${THISPKG_DIR}${sysroot}" install || exit 1
-${MAKE} distclean
-make_bin_archive v4e
-
-CFLAGS="$COMMON_CFLAGS" "$srcdir/configure" ${CONFIGURE_FLAGS}
-${MAKE} $JOBS || exit 1
-${MAKE} DESTDIR="${THISPKG_DIR}${sysroot}" install || exit 1
-#${MAKE} distclean
-make_bin_archive 000
+	${MAKE} $JOBS || exit 1
+	${MAKE} DESTDIR="${THISPKG_DIR}${sysroot}" install || exit 1
+	${MAKE} distclean
+	make_bin_archive $CPU
+done
 
 move_prefix
 configured_prefix="${prefix}"
